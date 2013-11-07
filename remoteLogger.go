73190f1752b5bd6ba1a1096fd691aa5e044e22e9
Philo Yang <mail@yangzhe1991.org>
2013-11-07 20:21:09 +0800

    log

--- remoteLogger.go
+++ remoteLogger.go
@@ -17,28 +17,24 @@ func (this *remoteLogger) Run(product string) {
 	addrs := strings.Split(this.addrs, ",")
 	index := 0
 	//fmt.Println("remote logger running")
-	timeout, _ := time.ParseDuration("500ms")
+	timeout, _ := time.ParseDuration("100ms")
 	for {
+		index = (index + 1) % len(addrs)
 		conn, err := net.DialTimeout("tcp", addrs[index], timeout)
 		if err != nil {
-			index = (index + 1) % len(addrs)
 			fmt.Println("remote logger connection build error!", err, addrs[index])
 			continue
 		}
 		//fmt.Println("remote logger conn build successful!")
 		fmt.Fprintln(conn, product)
-		counter := 100
 		for {
 			m := <-this.Chan
 			_, err := fmt.Fprintln(conn, m.Content)
-			fmt.Println("send one message")
 			if err != nil {
 				fmt.Println("remote logger send message err!", err)
 				break
 			}
-			if counter--; counter <= 0 {
-				break
-			}
+
 		}
 		//fmt.Println("remote logger conn close")
 		conn.Close()
